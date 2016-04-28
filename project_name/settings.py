e7546cfb24d255d60bdaac1b4273c1de62a52bd6
Aaron Madison <aaron.l.madison@gmail.com>
2016-04-28 06:24:22 -0500

    Peripheral: turned off debug

--- project_name/settings.py
+++ project_name/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qgw!j*bpxo7g&o1ux-(2ph818ojfj(3c#-#*_8r^8&hq5jg$3@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
