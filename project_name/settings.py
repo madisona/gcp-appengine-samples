198535e03d5763b23f7034ce19bcdda1404e75dc
Aaron Madison <aaron.l.madison@gmail.com>
2016-04-28 09:03:37 -0500

    Peripheral: Fixed standard env deploy

--- project_name/settings.py
+++ project_name/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
