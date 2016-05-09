4da427c663fbcba81130153db868952442a14826
Aaron Madison <aaron.l.madison@gmail.com>
2016-05-08 22:05:49 -0500

    Enhancement: Updated the logger to be in the format Error Reporting Requires

--- helloworld/gcp_logger.py
+++ helloworld/gcp_logger.py
@@ -1,15 +1,42 @@
 
 import logging
 import json
+import os
 
 
 class GCPJsonFormatter(logging.Formatter):
 
     def format(self, record):
+        # https://cloud.google.com/error-reporting/docs/formatting-error-messages
         return json.dumps({
-            'message': '{0}'.format(record.getMessage()),
-            'user': str(record.request.user),
+            'eventTime': record.created,
+            'message': self.formatException(record.exc_info),
             'level': record.levelname,
-            'traceback': self.formatException(record.exc_info),
-            'serviceContext': {'service': 'myapp'}
+            'serviceContext': {
+                'service': os.environ.get('GAE_MODULE_NAME', ''),
+                'version': os.environ.get('GAE_MODULE_VERSION', ''),
+            },
+            "context": {
+                "httpRequest": self._get_request_info(record),
+                'user': str(record.request.user) if record.request else "",
+                "reportLocation": {
+                    "filePath": record.pathname,
+                    "lineNumber": record.lineno,
+                    "functionName": record.funcName,
+                },
+            }
         })
+
+    def _get_request_info(self, record):
+        try:
+            request = record.request
+            return {
+                "method": request.method,
+                "url": request.get_full_path(),
+                "userAgent": request.META.get("HTTP_USER_AGENT", ""),
+                "referrer": request.META.get("HTTP_REFERER", ""),
+                "responseStatusCode": record.status_code,
+                "remoteIp": request.META.get("REMOTE_ADDR", "")
+            }
+        except Exception:
+            return {}
