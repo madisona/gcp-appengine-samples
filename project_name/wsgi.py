07830b7c3d1316e5a965db8702d3646529df52bb
Aaron Madison <aaron.l.madison@gmail.com>
2016-05-10 08:42:55 -0500

    Enhancement: Wiring up cloud debugger

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -13,4 +13,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
 
+try:
+  import googleclouddebugger
+  googleclouddebugger.AttachDebugger(
+      version=os.environ.get('GAE_MODULE_VERSION', ''),
+      module=os.environ.get('GAE_MODULE_NAME', ''),
+  )
+except ImportError:
+  pass
+
 application = get_wsgi_application()
